22ba77cbed78cabbb655bcc4c3f05f5deca051db
msBRF65 <msaito6525@gmail.com>
2023-07-06 15:21:17 +0900

    add: cgra page

---
+++ src/infrastructure/web/pages/cgraPage.tsx
@@ -0,0 +1,86 @@
+import '@/infrastructure/web/css/page.css';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { CGRAComponent } from '../components/CGRAComponent';
+import React, { useLayoutEffect, useRef, useState } from 'react';
+import { CGRALog } from '@/domain/entity';
+
+interface CGRAPageState {
+    cgraLog: CGRALog;
+}
+
+export const CGRAPage = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const state = location.state as CGRAPageState;
+    const cgraRef = useRef(null);
+    const [cycle, setCycle] = useState(1000);
+
+    const handleBackToHome = () => {
+        navigate('/');
+    };
+
+    const handleChangeCycle = (cycleStep: number) => {
+        setCycle(cycle + cycleStep);
+        if (cgraRef.current) {
+            let currentComponent: CGRAComponent = cgraRef.current;
+            currentComponent.handleCycleChange(cycle);
+        }
+    };
+
+    const [windowWidth, setWindowWidth] = React.useState(0);
+    const [windowHeight, setWindowHeight] = React.useState(0);
+    useLayoutEffect(() => {
+        (async () => {
+            let [width, height] = await Promise.all([window.electron.getWidth(), window.electron.getHeight()]);
+            setWindowHeight(height);
+            setWindowWidth(width);
+        })();
+    }, []);
+
+    return (
+        <div className="container">
+            <h1>CGRA Viewer</h1>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <CGRAComponent ref={cgraRef} width={windowWidth} height={windowHeight} cgraLog={state.cgraLog} />
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+                <button
+                    onClick={() => {
+                        handleChangeCycle(-10);
+                    }}
+                    style={{ width: '40px' }}
+                >
+                    -10
+                </button>
+                <button
+                    onClick={() => {
+                        handleChangeCycle(-1);
+                    }}
+                    style={{ width: '30px' }}
+                >
+                    -
+                </button>
+                <div style={{ width: '100px', textAlign: 'center' }}>Cycle: {cycle}</div>
+                <button
+                    onClick={() => {
+                        handleChangeCycle(1);
+                    }}
+                    style={{ width: '30px' }}
+                >
+                    +
+                </button>
+                <button
+                    onClick={() => {
+                        handleChangeCycle(10);
+                    }}
+                    style={{ width: '40px' }}
+                >
+                    +10
+                </button>
+            </div>
+            <button onClick={handleBackToHome} style={{ fontSize: '12pt' }}>
+                Back to Home
+            </button>
+        </div>
+    );
+};
