a8dbd3f14d6a0b22427be13c13cc655e30da0912
msBRF65 <msaito6525@gmail.com>
2023-07-06 15:18:00 +0900

    refactor: change CGRAUsecase func

--- src/applications/usecase/CGRAUsecase.ts
+++ src/applications/usecase/CGRAUsecase.ts
@@ -1,42 +1,25 @@
 import { CGRAConfig, CGRALog, PESignalNameConfig, PEValue } from '@/domain/entity';
 import { SignalData, VCDLoader } from '@/infrastructure/fileLoader';
 
-type updateCGRAInputType = {
+type createCGRAInputType = {
     path: string;
     cgraConfig: CGRAConfig;
     peConfigArray: PESignalNameConfig[][];
 };
-type updateCGRAOutputType = void;
-type getCGRADataInputType = {
-    cycle: number;
-};
-type getCGRADataOutputType = PEValue[][];
+type createCGRAOutputType = CGRALog;
 
 class CGRAUsecase {
-    private cgraLog: CGRALog;
+    constructor() {}
 
-    constructor() {
-        let defaultCGRAConfig: CGRAConfig = {
-            rowSize: 0,
-            columnSize: 0,
-            neighborPESize: 0,
-        };
-        let defaultPESignalNameConfigArray: PESignalNameConfig[][] = [];
-        this.cgraLog = new CGRALog(defaultCGRAConfig, defaultPESignalNameConfigArray);
-    }
-
-    public async updateCGRA(input: updateCGRAInputType): Promise<updateCGRAOutputType> {
+    public async createCGRA(input: createCGRAInputType): Promise<createCGRAOutputType> {
         let signalData: SignalData[] = await VCDLoader.getSignalData(input.path);
-        this.cgraLog = new CGRALog(input.cgraConfig, input.peConfigArray);
+        let cgraLog = new CGRALog(input.cgraConfig, input.peConfigArray);
 
         signalData.map((element) => {
-            console.log(element);
-            this.cgraLog.SetValueArray(element.module + '.' + element.signalName, element.waveDataArray);
+            cgraLog.SetValueArray(element.module + '.' + element.signalName, element.waveDataArray);
         });
-    }
 
-    public getCGRAData(input: getCGRADataInputType): getCGRADataOutputType {
-        return this.cgraLog.getValueByCycle(input.cycle);
+        return cgraLog;
     }
 }
 
