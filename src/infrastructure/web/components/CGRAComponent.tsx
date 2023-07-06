dfdc4d3b43aac20657d3d85abb18e792639a9a97
msBRF65 <msaito6525@gmail.com>
2023-07-06 15:21:05 +0900

    add: CGRA component

---
+++ src/infrastructure/web/components/CGRAComponent.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { PEComponent } from './PEComponent';
+import { CGRALog } from '@/domain/entity';
+
+interface ICGRAComponent {
+    width: number;
+    height: number;
+    cgraLog: CGRALog;
+}
+
+class CGRAComponent extends React.Component<ICGRAComponent, {}> {
+    readonly refArray: any[];
+
+    constructor(props: ICGRAComponent) {
+        super(props);
+        this.handleCycleChange = this.handleCycleChange.bind(this);
+        this.refArray = [];
+        for (let i = 0; i < this.props.cgraLog.cgraConfig.rowSize; i++) {
+            let refRowArray = [];
+            for (let j = 0; j < this.props.cgraLog.cgraConfig.columnSize; j++) {
+                refRowArray.push(React.createRef<PEComponent>());
+            }
+            this.refArray.push(refRowArray);
+        }
+    }
+
+    render() {
+        let peSize = Math.min(this.props.width, this.props.height) / 5;
+        return (
+            <div>
+                {Array.from({ length: this.props.cgraLog.cgraConfig.rowSize }, (_, i) => (
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        {Array.from({ length: this.props.cgraLog.cgraConfig.columnSize }, (_, j) => (
+                            <PEComponent
+                                ref={this.refArray[i][j]}
+                                size={peSize}
+                                rowId={i}
+                                columnId={j}
+                                peLog={this.props.cgraLog.getPELog(i, j)}
+                            />
+                        ))}
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+    handleCycleChange = (cycle: number) => {
+        this.refArray.map((refRowArray) => {
+            refRowArray.map((ref: any) => {
+                let tmpComponent: PEComponent = ref.current;
+                tmpComponent.handleChangeCycle(cycle);
+            });
+        });
+    };
+}
+
+export { CGRAComponent };
---
+++ src/infrastructure/web/components/PEComponent.tsx
@@ -0,0 +1,145 @@
+import '@/infrastructure/web/css/page.css';
+import { PELog, PEValue } from '@/domain/entity';
+import React from 'react';
+
+interface IPEComponent {
+    peLog: PELog;
+    size: number;
+    rowId: number;
+    columnId: number;
+}
+
+interface PEStateType {
+    peValue: PEValue;
+}
+
+class PEComponent extends React.Component<IPEComponent, PEStateType> {
+    constructor(props: IPEComponent) {
+        super(props);
+        this.state = {
+            peValue: this.props.peLog.getValueByCycle(0),
+        };
+        this.handleChangeCycle = this.handleChangeCycle.bind(this);
+    }
+
+    handleChangeCycle = (cycle: number) => {
+        console.log(this.props.peLog.getValueByCycle(cycle));
+        this.setState({
+            peValue: this.props.peLog.getValueByCycle(cycle),
+        });
+    };
+
+    render() {
+        const triangleStyle = {
+            borderTop: '10px solid #000000',
+            borderLeft: '5px solid transparent',
+            borderRight: '5px solid transparent',
+            position: 'absolute',
+        } as React.CSSProperties;
+
+        const lineStyle = {
+            backgroundColor: '#000000',
+            position: 'absolute',
+        } as React.CSSProperties;
+
+        const squareStyle = {
+            position: 'absolute',
+            top: '20%',
+            left: '20%',
+            width: '60%',
+            height: '60%',
+            backgroundColor: '#ffffff',
+            border: '2px solid #000000',
+            overflowWrap: 'break-word',
+            inlineSize: '60%',
+        } as React.CSSProperties;
+
+        const PEStyle = {
+            position: 'relative',
+            width: this.props.size,
+            height: this.props.size,
+            backgroundColor: '#ffffff',
+        } as React.CSSProperties;
+
+        return (
+            <div style={{ ...PEStyle }}>
+                {/* top */}
+                <div style={{ ...lineStyle, top: '0%', left: '45%', width: '2px', height: '20%' }}></div>
+                <div style={{ ...triangleStyle, top: 'calc(20% - 10px)', left: 'calc(45% - 4px)' }}></div>
+                <div style={{ ...lineStyle, top: '0%', left: '55%', width: '2px', height: '20%' }}></div>
+                {/* bottom */}
+                <div style={{ ...lineStyle, bottom: '0%', left: '45%', width: '2px', height: '20%' }}></div>
+                <div style={{ ...lineStyle, bottom: '0%', left: '55%', width: '2px', height: '20%' }}></div>
+                <div
+                    style={{
+                        ...triangleStyle,
+                        top: 'calc(80% + 2px)',
+                        left: 'calc(55% - 4px)',
+                        transform: 'rotate(-180deg)',
+                    }}
+                ></div>
+                {/* left */}
+                <div style={{ ...lineStyle, top: '45%', left: '0%', height: '2px', width: '20%' }}></div>
+                <div
+                    style={{
+                        ...triangleStyle,
+                        top: 'calc(45% - 4px)',
+                        left: 'calc(20% - 9px)',
+                        transform: 'rotate(-90deg)',
+                    }}
+                ></div>
+                <div style={{ ...lineStyle, top: '55%', left: '0%', height: '2px', width: '20%' }}></div>
+                {/* right */}
+                <div style={{ ...lineStyle, top: '45%', right: '0%', height: '2px', width: '20%' }}></div>
+                <div style={{ ...lineStyle, top: '55%', right: '0%', height: '2px', width: '20%' }}></div>
+                <div
+                    style={{
+                        ...triangleStyle,
+                        top: 'calc(55% - 4px)',
+                        left: 'calc(80% + 4px)',
+                        transform: 'rotate(90deg)',
+                    }}
+                ></div>
+                {/* output */}
+                <div style={{ top: 'calc(20% - 20px)', left: 'calc(55% + 10px)', position: 'absolute' }}>
+                    {this.state.peValue.outputValue}
+                </div>
+                <div style={{ bottom: 'calc(55% + 2px)', left: 'calc(80% + 10px)', position: 'absolute' }}>
+                    {this.state.peValue.outputValue}
+                </div>
+                <div style={{ top: 'calc(80% + 2px)', right: 'calc(55% + 10px)', position: 'absolute' }}>
+                    {this.state.peValue.outputValue}
+                </div>
+                <div style={{ top: 'calc(55% + 2px)', right: 'calc(80% + 10px)', position: 'absolute' }}>
+                    {this.state.peValue.outputValue}
+                </div>
+                {/* input */}
+                <div style={{ top: 'calc(20% - 20px)', right: 'calc(55% + 10px)', position: 'absolute' }}>
+                    {this.state.peValue.inputValueArray[0]}
+                </div>
+                <div style={{ top: 'calc(55% + 2px)', left: 'calc(80% + 10px)', position: 'absolute' }}>
+                    {this.state.peValue.inputValueArray[1]}
+                </div>
+                <div style={{ top: 'calc(80% + 2px)', left: 'calc(55% + 10px)', position: 'absolute' }}>
+                    {this.state.peValue.inputValueArray[2]}
+                </div>
+                <div style={{ bottom: 'calc(55% + 2px)', right: 'calc(80% + 10px)', position: 'absolute' }}>
+                    {this.state.peValue.inputValueArray[3]}
+                </div>
+                {/* square */}
+                <div style={{ ...squareStyle }}>
+                    PE({this.props.rowId}, {this.props.columnId})
+                    {Object.keys(this.state.peValue.statusValueMap).map((key) => {
+                        return (
+                            <div style={{ whiteSpace: 'pre-wrap', fontSize: "12px" }}>
+                                {key}: {this.state.peValue.statusValueMap[key]}
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
+export { PEComponent };
