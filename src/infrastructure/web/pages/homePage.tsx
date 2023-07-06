897a23b1ddd94190ab4d229174ebbe625ad7b869
msBRF65 <msaito6525@gmail.com>
2023-07-06 22:57:30 +0900

    add: file input

--- src/infrastructure/web/pages/homePage.tsx
+++ src/infrastructure/web/pages/homePage.tsx
@@ -2,19 +2,19 @@ import '@/infrastructure/web/css/page.css';
 import { CGRAUsecase } from '@/applications/usecase';
 import { CGRAConfigLoader } from '@/infrastructure/fileLoader';
 import { useLocation, useNavigate } from 'react-router-dom';
-
-let vcdPath: string = '/Users/saitoumakoto/Desktop/cgra.vcd';
-let configPath: string = '/Users/saitoumakoto/Desktop/cgra.json';
+import { useState } from 'react';
 
 export const HomePage = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const cgraUsecase = new CGRAUsecase();
+    const [vcdFilePath, setVcdFilePath] = useState('');
+    const [configFilePath, setConfigFilePath] = useState('');
 
-    const handleClick = async (configPath: string) => {
-        var cgraConfigData = await CGRAConfigLoader.getCGRAConfig(configPath);
+    const handleClick = async () => {
+        var cgraConfigData = await CGRAConfigLoader.getCGRAConfig(configFilePath);
         let cgraLog = await cgraUsecase.createCGRA({
-            path: vcdPath,
+            path: vcdFilePath,
             cgraConfig: cgraConfigData.config,
             peConfigArray: cgraConfigData.peConfigArray,
         });
@@ -24,11 +24,37 @@ export const HomePage = () => {
     return (
         <div className="container">
             <h1>CGRA Debugger Home</h1>
-            <div>{vcdPath}</div>
+            <div>
+                <span style={{ marginLeft: '126px' }}>vcd File: </span>
+                <input
+                    type="file"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        const files = event.currentTarget.files;
+                        if (!files || files?.length === 0) return;
+                        const file = files[0];
+                        setVcdFilePath(file.path);
+                    }}
+                />
+            </div>
+            <div>
+                <span style={{ marginRight: '5px' }}>configuration JSON File:</span>
+                <input
+                    type="file"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        const files = event.currentTarget.files;
+                        if (!files || files?.length === 0) return;
+                        const file = files[0];
+                        setConfigFilePath(file.path);
+                    }}
+                />
+            </div>
+
+            <br></br>
             <button
                 onClick={async () => {
-                    return handleClick(configPath);
+                    return handleClick();
                 }}
+                style={{ width: '100px', height: '30px', fontSize: '15px' }}
             >
                 Read File
             </button>
