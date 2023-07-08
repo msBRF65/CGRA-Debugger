124128da6779165361c3962b3234277c45cf43a5
msBRF65 <msaito6525@gmail.com>
2023-07-08 19:41:10 +0900

    fix: tmp delete apple auth

--- forge.config.js
+++ forge.config.js
@@ -1,12 +1,6 @@
 module.exports = {
     packagerConfig: {
         asar: true,
-        osxNotarize: {
-            tool: 'notarytool',
-            appleId: process.env.APPLE_ID,
-            appleIdPassword: process.env.APPLE_PASSWORD,
-            teamId: process.env.APPLE_TEAM_ID,
-        },
     },
     rebuildConfig: {},
     makers: [
