ffd7d57788bac08869841fae1b1d22fa51225a0c
msBRF65 <msaito6525@gmail.com>
2023-07-23 19:08:14 +0900

    add: cgra configuration data creator as service

---
+++ src/domain/service/cgraConfigurationDataCreator.ts
@@ -0,0 +1,68 @@
+import { CGRAConfig, CGRAConfigurationData } from '@/domain/entity';
+import { CGRAConfigId } from '../valueObject';
+
+type PEConfigType = {
+    row_id: string;
+    column_id: string;
+    config: ConfigType[];
+};
+type ConfigType = {
+    context_id: string;
+    operation_type: string;
+    operation_name: string;
+    const_value: string;
+    to_config_id: ConfigIdType[];
+    from_config_id: ConfigIdType[];
+};
+type ConfigIdType = { row_id: string; column_id: string; context_id: string };
+
+class CGRAConfigurationDataCreator {
+    public static createCGRAConfigurationData(
+        cgraConfig: CGRAConfig,
+        PEConfigArray: PEConfigType[],
+    ): CGRAConfigurationData {
+        let cgraConfiguraionData = new CGRAConfigurationData(cgraConfig);
+        PEConfigArray.map((PEConfig: PEConfigType) => {
+            PEConfig.config.map((config: ConfigType) => {
+                let configId = new CGRAConfigId(
+                    Number(PEConfig.row_id),
+                    Number(PEConfig.column_id),
+                    Number(config.context_id),
+                );
+                let fromConfigIdArray: CGRAConfigId[] = [];
+                config.from_config_id.map((fromConfigId: ConfigIdType) => {
+                    fromConfigIdArray.push(
+                        new CGRAConfigId(
+                            Number(fromConfigId.row_id),
+                            Number(fromConfigId.column_id),
+                            Number(fromConfigId.context_id),
+                        ),
+                    );
+                });
+                let toConfigIdArray: CGRAConfigId[] = [];
+                config.to_config_id.map((toConfigId: ConfigIdType) => {
+                    toConfigIdArray.push(
+                        new CGRAConfigId(
+                            Number(toConfigId.row_id),
+                            Number(toConfigId.column_id),
+                            Number(toConfigId.context_id),
+                        ),
+                    );
+                });
+                cgraConfiguraionData.SetCGRAConfigurationData({
+                    configId: configId,
+                    configurationData: {
+                        constValue: Number(config.const_value),
+                        fromConfigIdArray: fromConfigIdArray,
+                        toConfigIdArray: toConfigIdArray,
+                        operation: config.operation_type,
+                    },
+                });
+            });
+        });
+
+        return cgraConfiguraionData;
+    }
+}
+
+export { CGRAConfigurationDataCreator, PEConfigType };
