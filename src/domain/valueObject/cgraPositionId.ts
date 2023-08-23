b925bddf2bda81d4fa936feae836c756ebc00a86
msBRF65 <msaito6525@gmail.com>
2023-08-23 18:13:40 +0900

    add: CGRAPositionId operator

--- src/domain/valueObject/cgraPositionId.ts
+++ src/domain/valueObject/cgraPositionId.ts
@@ -6,6 +6,14 @@ class CGRAPositionId {
         this.rowId = rowId;
         this.columnId = columnId;
     }
+
+    public add(positionId: CGRAPositionId): CGRAPositionId {
+        return new CGRAPositionId(this.rowId + positionId.rowId, this.columnId + positionId.columnId);
+    }
+
+    public equal(positionId: CGRAPositionId): boolean {
+        return this.rowId === positionId.rowId && this.columnId === positionId.columnId;
+    }
 }
 
 export { CGRAPositionId };
