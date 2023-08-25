3103e5de62b7857f821585dcf7bde66738aecce8
msBRF65 <msaito6525@gmail.com>
2023-08-25 15:55:42 +0900

    add: operation to verify

--- src/domain/service/logChecker.ts
+++ src/domain/service/logChecker.ts
@@ -1,9 +1,26 @@
 import { ConfigurationData, PELog } from '@/domain/entity';
 import { OperationExecutor } from './operationExecutor';
-import { CGRAPositionId } from '../valueObject';
+import { CGRAPositionId, Operation, OperationTypeEnum } from '../valueObject';
 import { exec } from 'child_process';
 
 class LogChecker {
+    private static OperaionToVerify(op: Operation) {
+        switch (op.operationType) {
+            case OperationTypeEnum.ADD:
+                return true;
+            case OperationTypeEnum.SUB:
+                return true;
+            case OperationTypeEnum.MUL:
+                return true;
+            case OperationTypeEnum.DIV:
+                return true;
+            case OperationTypeEnum.OUTPUT:
+                return true;
+            case OperationTypeEnum.ROUTE:
+                return true;
+        }
+        return false;
+    }
     static VerifyOperationResult(
         peLog: PELog,
         peConfigurationData: ConfigurationData[],
@@ -17,6 +34,9 @@ class LogChecker {
         const exec_pe_value = peLog.getValueByCycle(exec_time);
 
         const exec_config = peConfigurationData[exec_pe_value.aluConfigId];
+        if (!this.OperaionToVerify(exec_config.operation)) {
+            return true;
+        }
 
         let input_data_1: number = 0;
         let input_data_2: number = 0;
@@ -30,6 +50,14 @@ class LogChecker {
                 input_data_2 = input_pe_value.inputValueArray[index];
             }
         });
+
+        if (exec_config.fromConfigIdArray.length >= 1) {
+            exec_config.fromConfigIdArray[0].cgraPositionId.equal(peLog.positionId);
+            input_data_1 = output_pe_value.outputValue;
+        } else if (exec_config.fromConfigIdArray.length == 2) {
+            exec_config.fromConfigIdArray[1].cgraPositionId.equal(peLog.positionId);
+            input_data_2 = output_pe_value.outputValue;
+        }
 
         let output_data = output_pe_value.outputValue;
         let result = OperationExecutor.ExecuteOperation(exec_config.operation, input_data_1, input_data_2);
